d30a0beac37f46f12cb5772e7bc8e5bfcdc8a8e8
Acácio Centeno <acaciocenteno@gmail.com>
2015-02-01 10:35:16 -0200

    Minor fixes on install, add cherry-pick to gitconfig

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@ find . -type f -name '.*' -exec cp {} $HOME \;
 cp -R .vim ~
 cd ~
 source .bashrc
+
+OS=`uname -s`
+if [ "${OS}" == "Darwin" ]; then
+	grep "source ~/.bashrc" ~/.bash_profile >/dev/null 2>&1
+	if [ "$?" == "1" ]; then
+	   echo "source ~/.bashrc" >> ~/.bash_profile
+	fi	   
+fi
