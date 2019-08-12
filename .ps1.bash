32822a4b103bca47f655026a1cb94c35d756e55a
Acacio <acacio.centeno@gmail.com>
2019-08-12 14:27:11 +0200

    Some tweaks

--- .ps1.bash
+++ .ps1.bash
@@ -23,4 +23,21 @@ workdir_color="\e[1;38;05;31m"
 gitbranch_color="\e[0;38;05;144m"
 branch='$(__git_ps1 " (%s)")'
 
-export PS1="${user_color}\u\e[m@\h ${workdir_color}\w\e[m${gitbranch_color}${branch}\e[m\n\\$ "
+status() {
+    RC=$?
+    [[ ${RC} != 0 ]] && echo -ne "\e[0;38;05;124m ${RC}\e[m"
+}
+
+user_and_host_when_not_in_tmux() {
+    if [[ -n ${TMUX} ]]; then
+        PS1_HOSTNAME=
+    else
+        PS1_HOSTNAME="${user_color}\u\e[m@\h "
+    fi
+}
+
+user_and_host_when_not_in_tmux
+
+export PS1_HOSTDIR="${workdir_color}\w\e[m"
+export PS1_GIT_BRANCH="${gitbranch_color}${branch}\e[m"
+export PS1="${PS1_HOSTNAME}${PS1_HOSTDIR}${PS1_GIT_BRANCH}\$(status)\n\\$ "
